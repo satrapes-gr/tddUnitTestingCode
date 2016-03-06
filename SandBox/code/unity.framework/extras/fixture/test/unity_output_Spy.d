df3ce7fe3e64e06e68324530b7d79730304ff944
satrapes <satrapes@hotmail.com>
2016-03-06 13:40:43 +0000

    Revert "First commit"
    This reverts commit 45fd04a94bb7c78f7c8f160d6c2908d55a095834.

--- SandBox/code/unity.framework/extras/fixture/test/unity_output_Spy.d
+++
@@ -1,8 +0,0 @@
-objs/../code/unity.framework/extras/fixture/test/unity_output_Spy.o: \
- ../code/unity.framework/extras/fixture/test/unity_output_Spy.c \
- ../code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h \
- ../code/unity.framework/extras/fixture/test/unity_output_Spy.h
-
-../code/unity.framework/extras/fixture/src/unity_fixture_malloc_overrides.h:
-
-../code/unity.framework/extras/fixture/test/unity_output_Spy.h:
--- code/Makefile
+++
@@ -1,7 +0,0 @@
-all:
-	make -f MakefileCppUTest.mk
-	make -f MakefileUnity.mk 
-
-clean:
-	make -f MakefileCppUTest.mk clean
-	make -f MakefileUnity.mk  clean
